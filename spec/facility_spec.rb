0f88efbf1a563d2fd38ffdd38cada68d12a53160
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-07-13 18:09:31 -0400

    Finishing up the tests for renew license

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -121,19 +121,25 @@ RSpec.describe Facility do
     end
     
     describe 'Renew license' do 
-      it 'renews license dor qualified registrants' do 
+      it 'renews license for qualified registrants' do 
         @registrant_1 = Registrant.new('Bruce', 18, true)
         @registrant_2 = Registrant.new('Penny', 16)
         @registrant_2.earn_permit
-        facility_1.add_service('Written test')
+        facility_1.add_service('Written Test')
         facility_1.add_service('Road Test')
-        facility_1.add_service('Renew license')
+        facility_1.add_service('Renew License')
         facility_1.administer_written_test(@registrant_1)
         facility_1.administer_written_test(@registrant_2)
         facility_1.administer_road_test(@registrant_1)
         facility_1.administer_road_test(@registrant_2)
         expect(facility_1.renew_drivers_license(@registrant_1)).to be true
         expect(facility_1.renew_drivers_license(@registrant_2)).to be true
+      end
+
+      it 'does not renew license' do 
+        @registrant_3 = Registrant.new('Tucker', 15)
+        expect(facility_1.renew_drivers_license(@registrant_3)).to eq false
+        expect(@registrant_3.license_data[:renewed]).to be false
       end
     end
     
