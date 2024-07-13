76955a615aec045fdb6e16bdf3406b1365ed44e2
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-07-12 21:45:02 -0400

    Made my register_vehicle method dynaic

--- lib/facility.rb
+++ lib/facility.rb
@@ -18,4 +18,24 @@ class Facility
   def add_service(service)
     @services << service
   end
+
+  def register_vehicle(vehicle)
+    return nil unless @services.include?('Vehicle Registration')
+    @registered_vehicles << vehicle
+
+    if vehicle.antique?
+      @collected_fees += 25
+      vehicle.plate_type = :antique
+    elsif vehicle.electric_vehicle?
+      @collected_fees += 200
+      vehicle.plate_type = :ev
+    else
+      @collected_fees += 100
+      vehicle.plate_type = :regular
+    end
+
+    @collected_fees
+    
+  end
+  
 end
