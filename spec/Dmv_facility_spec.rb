5404e598f3c3d83882759e4da2c3843c16278c60
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-07-14 11:01:03 -0400

    Created test for Iterating through the keys

--- spec/Dmv_facility_spec.rb
+++ spec/Dmv_facility_spec.rb
@@ -12,5 +12,21 @@ RSpec.describe DmvFacilities do
     facilities = @factory.create_facilities(@co_dmv_office_locations)
       expect(facilities).to be_an(Array)
       expect(facilities.length).to eq(@co_dmv_office_locations.length)
+      facilities.each do |facility|
+        expect(facility).to be_a(DmvFacility)
+        expect(facility.dmv_id).to be_truthy
+        expect(facility.dmv_office).to be_truthy
+        expect(facility.address_li).to be_truthy
+        expect(facility.address_1).to be_truthy
+        expect(facility.city).to be_truthy
+        expect(facility.state).to be_truthy
+        expect(facility.zip).to be_truthy
+        expect(facility.phone).to be_truthy
+        expect(facility.hours).to be_truthy
+        expect(facility.services_p).to be_truthy
+        expect(facility.parking_no).to be_truthy
+        expect(facility.photo).to be_truthy
+        expect(facility.address_id).to be_truthy
+      end
   end
 end
