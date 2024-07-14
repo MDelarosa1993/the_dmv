86409364bea2b680141fa96f995c4c330207bbef
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-07-14 18:19:30 -0400

    Added methods to CO, NY, MI tests

--- lib/Dmv_Facility_Factory.rb
+++ lib/Dmv_Facility_Factory.rb
@@ -1,5 +1,5 @@
 class DmvFacilityFactory 
-  def create_facilities(co_dmv_office_locations)
+  def colorado_facilities(co_dmv_office_locations)
 
       facilities = co_dmv_office_locations.map do |co_dmv_office_location|
       name = co_dmv_office_location[:dmv_office]
@@ -9,4 +9,28 @@ class DmvFacilityFactory
     end
    facilities
   end
+
+  def new_york_facilities(new_york_facilities)
+      facilities = new_york_facilities.map do |new_york_facility|
+      name = new_york_facility[:office_name]
+      address = "#{new_york_facility[:street_address_line_1]}, #{new_york_facility[:city]}, #{new_york_facility[:state]}, #{new_york_facility[:zip_code]}"
+      phone = if new_york_facility[:public_phone_number]
+        new_york_facility[:public_phone_number]
+      else
+       "Not available"
+      end
+      Facility.new({name: name, address: address, phone: phone})  
+    end
+    facilities
+  end
+
+  def missouri_facilities(missouri_facilities)
+    facilities = missouri_facilities.map do |new_york_facility|
+    name = new_york_facility[:office_name]
+    address = "#{[]}, #{[]}, #{[]}, #{[]}"
+    phone = new_york_facility[:public_phone_number]
+    Facility.new({name: name, address: address, phone: phone})  
+  end
+  facilities
+end
 end
