997741c95bfdf7e8084667191e04bd2ecd61320a
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-07-15 17:53:39 -0400

    Commented out CO data

--- lib/Dmv_Facility_Factory.rb
+++ lib/Dmv_Facility_Factory.rb
@@ -1,15 +1,15 @@
 class DmvFacilityFactory 
-  def colorado_facilities(co_dmv_office_locations)
+  # def colorado_facilities(co_dmv_office_locations)
 
-      facilities = co_dmv_office_locations.map do |co_dmv_office_location|
-      co_dmv_office_location.default = "N/A"
-      name = co_dmv_office_location[:dmv_office]
-      address = "#{co_dmv_office_location[:address_li]}, #{co_dmv_office_location[:address__1]}, #{co_dmv_office_location[:city]}, #{co_dmv_office_location[:state]} #{co_dmv_office_location[:zip]}"
-      phone = co_dmv_office_location[:phone]
-      Facility.new({name: name, address: address, phone: phone})
-    end
-   facilities
-  end
+  #     facilities = co_dmv_office_locations.map do |co_dmv_office_location|
+  #     co_dmv_office_location.default = "N/A"
+  #     name = co_dmv_office_location[:dmv_office]
+  #     address = "#{co_dmv_office_location[:address_li]}, #{co_dmv_office_location[:address__1]}, #{co_dmv_office_location[:city]}, #{co_dmv_office_location[:state]} #{co_dmv_office_location[:zip]}"
+  #     phone = co_dmv_office_location[:phone]
+  #     Facility.new({name: name, address: address, phone: phone})
+  #   end
+  #  facilities
+  # end
 
   def new_york_facilities(new_york_facilities)
       facilities = new_york_facilities.map do |new_york_facility|
