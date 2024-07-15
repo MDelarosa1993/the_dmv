13efbb986b0dfc285be5f211e959e30f726dc298
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-07-15 17:54:04 -0400

    Commented out CO data

--- spec/Dmv_Facility_Factory_spec.rb
+++ spec/Dmv_Facility_Factory_spec.rb
@@ -9,19 +9,19 @@ RSpec.describe DmvFacilityFactory do
      @new_york_facilities = DmvDataService.new.ny_dmv_office_locations
      @missouri_facilities = DmvDataService.new.mo_dmv_office_locations
     end 
-    describe 'creates facilities ' do 
-      it 'creates Colorado objects' do 
-        facilities = DmvFacilityFactory.new.colorado_facilities(@co_dmv_office_locations)
-        expect(facilities).to be_an_instance_of(Array) 
-        expect(facilities.length).to eq(@co_dmv_office_locations.length)
-        facilities.each do |facility|
-          expect(facility).to be_a(Facility) 
-          expect(facility.name).to be_truthy
-          expect(facility.address).to be_truthy
-          expect(facility.phone).to be_truthy
-      end
-    end
-  end
+    # describe 'creates facilities ' do 
+    #   it 'creates Colorado objects' do 
+    #     facilities = DmvFacilityFactory.new.colorado_facilities(@co_dmv_office_locations)
+    #     expect(facilities).to be_an_instance_of(Array) 
+    #     expect(facilities.length).to eq(@co_dmv_office_locations.length)
+    #     facilities.each do |facility|
+    #       expect(facility).to be_a(Facility) 
+    #       expect(facility.name).to be_truthy
+    #       expect(facility.address).to be_truthy
+    #       expect(facility.phone).to be_truthy
+    #   end
+    # end
+  # end
       describe 'Creates NY objects'
         it 'creates NY facilites' do 
           facilities = DmvFacilityFactory.new.new_york_facilities(@new_york_facilities)
