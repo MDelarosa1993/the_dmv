9369fc0e9b874c2413fa0c40eee32fb7f2fa7eb8
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-07-15 17:52:43 -0400

    refactored code, removed the let hooks for before hooks and added @'s

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -5,76 +5,67 @@ require './lib/registrant'
 require 'date'
 
 RSpec.describe Facility do
-  let(:facility_1) do
-    Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
-    
-  end
+  before(:each) do
+  @facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+  
+  @facility_2 = Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
 
-  let(:facility_2) do
-    Facility.new({name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239', phone: '(720) 865-4600'})
-  end
+  @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice, plate_type: :regular})
 
-  let(:cruz) do
-    Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice, plate_type: :regular})
-  end
+  @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev})
 
-  let(:bolt) do
-    Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev})
-  end
+  @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice})
+end
 
-  let(:camaro) do
-    Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice})
-  end
 
   describe '#initialize' do
     it 'initializes with correct attributes' do
-      expect(facility_1.name).to eq('DMV Tremont Branch')
-      expect(facility_1.address).to eq('2855 Tremont Place Suite 118 Denver CO 80205')
-      expect(facility_1.phone).to eq('(720) 865-4600')
-      expect(facility_1.services).to eq([])
-      expect(facility_1.collected_fees).to eq(0)
+      expect(@facility_1.name).to eq('DMV Tremont Branch')
+      expect(@facility_1.address).to eq('2855 Tremont Place Suite 118 Denver CO 80205')
+      expect(@facility_1.phone).to eq('(720) 865-4600')
+      expect(@facility_1.services).to eq([])
+      expect(@facility_1.collected_fees).to eq(0)
     end
   end
 
   describe '#add_service' do
     it 'adds a service to the facility' do
-      facility_1.add_service('Vehicle Registration')
-      expect(facility_1.services).to include('Vehicle Registration')
+      @facility_1.add_service('Vehicle Registration')
+      expect(@facility_1.services).to include('Vehicle Registration')
     end
   end
 
   describe '#register_vehicle' do
       it 'registers a vehicle with plate_type :regular' do 
-        facility_1.add_service('Vehicle Registration')
-        facility_1.register_vehicle(cruz)
-        expect(cruz.registration_date).to eq(nil)
-        expect(cruz.plate_type).to eq(:regular)
-        expect(facility_1.registered_vehicles).to include(cruz)
-        expect(facility_1.collected_fees).to eq(100)
+        @facility_1.add_service('Vehicle Registration')
+        @facility_1.register_vehicle(@cruz)
+        expect(@cruz.registration_date).to eq(nil)
+        expect(@cruz.plate_type).to eq(:regular)
+        expect(@facility_1.registered_vehicles).to include(@cruz)
+        expect(@facility_1.collected_fees).to eq(100)
       end
 
       it 'registers plate_type :antique' do 
-        facility_1.add_service('Vehicle Registration')
-        facility_1.register_vehicle(camaro)
-        expect(camaro.registration_date).to eq(nil)
-        expect(camaro.plate_type).to eq(:antique)
-        expect(facility_1.registered_vehicles).to include(camaro)
-        expect(facility_1.collected_fees).to eq(25)
+        @facility_1.add_service('Vehicle Registration')
+        @facility_1.register_vehicle(@camaro)
+        expect(@camaro.registration_date).to eq(nil)
+        expect(@camaro.plate_type).to eq(:antique)
+        expect(@facility_1.registered_vehicles).to include(@camaro)
+        expect(@facility_1.collected_fees).to eq(25)
       end
   
 
       it 'registers a third vehicle with plate_type :ev' do
-        facility_1.add_service('Vehicle Registration')
-        facility_1.register_vehicle(bolt)
-        expect(bolt.registration_date).to eq(nil)
-        expect(bolt.plate_type).to eq(:ev)
-        expect(facility_1.registered_vehicles).to include(bolt)
-        expect(facility_1.collected_fees).to eq(200)
+        @facility_1.add_service('Vehicle Registration')
+        @facility_1.register_vehicle(@bolt)
+        expect(@bolt.plate_type).to eq(:ev)
+        expect(@facility_1.registered_vehicles).to include(@bolt)
+        expect(@facility_1.collected_fees).to eq(200)
       end
         it 'does not register a vehicle' do
-        expect(facility_2.register_vehicle(bolt)).to eq(nil)
-        expect(facility_2.registered_vehicles).to eq([])
-        expect(facility_2.collected_fees).to eq(0)
+        expect(@facility_2.register_vehicle(@bolt)).to eq(nil)
+        expect(@facility_2.registered_vehicles).to eq([])
+        expect(@facility_2.collected_fees).to eq(0)
       end
   end
 
@@ -82,16 +73,16 @@ RSpec.describe Facility do
       it 'allows eligible registrants to take the written test' do
         @registrant_1 = Registrant.new('Bruce', 18, true)
         @registrant_2 = Registrant.new('Penny', 16)
-        facility_1.add_service('Written Test')
-        expect(facility_1.administer_written_test(@registrant_1)).to be true
-        expect(facility_1.administer_written_test(@registrant_1)).to be true
+        @facility_1.add_service('Written Test')
+        expect(@facility_1.administer_written_test(@registrant_1)).to be true
+        expect(@facility_1.administer_written_test(@registrant_1)).to be true
         expect(@registrant_1.license_data[:written]).to be true
         expect(@registrant_1.license_data[:written]).to be true
       end
   
       it 'does not allow ineligible registrants to take the written test' do
         @registrant_3 = Registrant.new('Tucker',15)
-        expect(facility_1.administer_written_test(@registrant_2)).to be false
+        expect(@facility_1.administer_written_test(@registrant_2)).to be false
         expect(@registrant_3.license_data[:written]).to be false
       end
     end
@@ -101,12 +92,12 @@ RSpec.describe Facility do
         @registrant_1 = Registrant.new('Bruce', 18, true)
         @registrant_2 = Registrant.new('Penny', 16)
         @registrant_2.earn_permit
-        facility_1.add_service('Written Test')
-        facility_1.add_service('Road Test')
-        facility_1.administer_written_test(@registrant_1)
-        facility_1.administer_written_test(@registrant_2)
-        facility_1.administer_road_test(@registrant_1)
-        facility_1.administer_road_test(@registrant_2)
+        @facility_1.add_service('Written Test')
+        @facility_1.add_service('Road Test')
+        @facility_1.administer_written_test(@registrant_1)
+        @facility_1.administer_written_test(@registrant_2)
+        @facility_1.administer_road_test(@registrant_1)
+        @facility_1.administer_road_test(@registrant_2)
         expect(@registrant_1.license_data[:license]).to be true
         expect(@registrant_2.license_data[:license]).to be true
      
@@ -114,8 +105,8 @@ RSpec.describe Facility do
 
      it 'does not take road test for unqualified registrant' do 
       @registrant_3 = Registrant.new('Tucker', 15)
-      facility_1.administer_written_test(@registrant_2)
-      expect(facility_1.administer_road_test(@registrant_2)).to be false
+      @facility_1.administer_written_test(@registrant_2)
+      expect(@facility_1.administer_road_test(@registrant_2)).to be false
       expect(@registrant_3.license_data[:license]).to be false
      end
     end
@@ -125,20 +116,20 @@ RSpec.describe Facility do
         @registrant_1 = Registrant.new('Bruce', 18, true)
         @registrant_2 = Registrant.new('Penny', 16)
         @registrant_2.earn_permit
-        facility_1.add_service('Written Test')
-        facility_1.add_service('Road Test')
-        facility_1.add_service('Renew License')
-        facility_1.administer_written_test(@registrant_1)
-        facility_1.administer_written_test(@registrant_2)
-        facility_1.administer_road_test(@registrant_1)
-        facility_1.administer_road_test(@registrant_2)
-        expect(facility_1.renew_drivers_license(@registrant_1)).to be true
-        expect(facility_1.renew_drivers_license(@registrant_2)).to be true
+        @facility_1.add_service('Written Test')
+        @facility_1.add_service('Road Test')
+        @facility_1.add_service('Renew License')
+        @facility_1.administer_written_test(@registrant_1)
+        @facility_1.administer_written_test(@registrant_2)
+        @facility_1.administer_road_test(@registrant_1)
+        @facility_1.administer_road_test(@registrant_2)
+        expect(@facility_1.renew_drivers_license(@registrant_1)).to be true
+        expect(@facility_1.renew_drivers_license(@registrant_2)).to be true
       end
 
       it 'does not renew license' do 
         @registrant_3 = Registrant.new('Tucker', 15)
-        expect(facility_1.renew_drivers_license(@registrant_3)).to eq false
+        expect(@facility_1.renew_drivers_license(@registrant_3)).to eq false
         expect(@registrant_3.license_data[:renewed]).to be false
       end
     end
